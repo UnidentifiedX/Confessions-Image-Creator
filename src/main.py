9297f139ee85efdbefb0f4705fe250bccf90cb70
Sun Zizhuo <sunzizhuo33@gmail.com>
2022-10-14 14:14:45 +0800

    added converting excel file entries to confession images

--- src/main.py
+++ src/main.py
@@ -6,11 +6,21 @@ from pandas import ExcelFile
 
 dirname = os.path.dirname(__file__)
 
-data = pd.read_excel(os.path.join(dirname, r".\files\test.xlsx"), sheet_name="Sheet1")
+data = pd.read_excel(os.path.join(dirname, r".\files\test.xlsx"), sheet_name="Sheet1").to_dict()
 
-hti = Html2Image(output_path=os.path.join(dirname, "runs"))
-hti.screenshot(
-    html_file=os.path.join(dirname, "mimic.html"),
-    save_as='woah.png',
-    size=(500, 500)
-)
+hti = Html2Image(output_path=os.path.join(dirname, "../runs"))
+
+html = open(os.path.join(dirname, "mimic.html"), "r", encoding="utf8").read()
+
+count = 0
+for i in data['your confession ^^']:
+    confession = data["your confession ^^"][i]
+    new_html = html.replace("your_confession_here", confession)
+
+    hti.screenshot(
+        html_str=new_html,
+        save_as=f'{count}.png',
+        size=(500, 500)
+    )
+
+    count += 1
